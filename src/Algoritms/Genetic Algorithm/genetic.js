6e1a76396ffbd33c92b5e85036c84ec01b0ba154
Mehrnoosh yaghoubi <mehrnooshyaghoubii@gmail.com>
2025-04-04 10:20:37 -0400

    genetic

---
+++ src/Algoritms/Genetic Algorithm/genetic.js
@@ -0,0 +1,13 @@
+class GeneticAlgorithm {
+  constructor(populationSize, mutationRate, crossoverRate) {
+    this.populationSize = populationSize;
+    this.mutationRate = mutationRate;
+    this.crossoverRate = crossoverRate;
+    this.population = [];
+  }}
+
+
+
+
+
+
